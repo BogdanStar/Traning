5a0ac9bee14e9d151b6a4b5486a8299b1bdac548
BogdanStar <starwars199418@gmail.com>
2022-05-07 00:07:10 +0300

    BadBoy

--- Traning main/js-traning.js
+++ Traning main/js-traning.js
@@ -3,17 +3,21 @@
 const form = document.querySelector(".js-form");
 const output = document.querySelector(".output");
 
+const data = [];
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const number = this.elements["number"];
-  const content = this.elements["content"];
-  dataCreator(number.value, content.value);
+  const number = this.elements["number"].value;
+  const content = this.elements["content"].value;
+  data.push({ number, content });
+  data.sort((a, b) => a.number - b.number);
+  dataCreator();
   this.reset();
 });
 
-function dataCreator(number, content) {
-  const data = document.createElement("div");
-
-  data.innerHTML = `${number}) ${content}`;
-  output.appendChild(data);
+function dataCreator() {
+  const list = data.map((note) => {
+    return `<div>${note.number}) ${note.content}</div>`;
+  });
+  output.innerHTML = list.join("");
 }
