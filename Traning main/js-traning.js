3c6ae13b79c6e7ab6fb83a5011efc2dc9bc006b8
BogdanStar <starwars199418@gmail.com>
2022-05-07 00:01:36 +0300

    BedBoy

--- Traning main/js-traning.js
+++ Traning main/js-traning.js
@@ -1,2 +1,19 @@
-alert( 'Я JavaScript!' )
-alert('Привет')
+
+
+const form = document.querySelector(".js-form");
+const output = document.querySelector(".output");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const number = this.elements["number"];
+  const content = this.elements["content"];
+  dataCreator(number.value, content.value);
+  this.reset();
+});
+
+function dataCreator(number, content) {
+  const data = document.createElement("div");
+
+  data.innerHTML = `${number}) ${content}`;
+  output.appendChild(data);
+}
