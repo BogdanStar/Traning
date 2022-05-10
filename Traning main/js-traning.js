ca9a1be03cd1000e8ac4645f800876a2cc1533ed
BogdanStar <starwars199418@gmail.com>
2022-05-10 13:32:23 +0300

    Новые фичи

--- Traning main/js-traning.js
+++ Traning main/js-traning.js
@@ -7,10 +7,31 @@ const data = [];
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const number = this.elements["number"].value;
-  const content = this.elements["content"].value;
-  data.push({ number, content });
-  data.sort((a, b) => a.number - b.number);
+  let number = +this.elements["number"].value;
+   if(!number){
+      alert("Введите число")
+      return
+   }
+   if (number<1){
+      alert ("Введите целое положительное число")
+      return
+   }
+  let content = this.elements["content"].value;
+   if(content === ""){
+      alert("Введите данные")
+      return
+   }
+    let index = null;
+    for (let i = 0; i < data.length; i++){
+       if (data[i].number === number){
+          index = String(i);
+          alert("Нельзя использовать то же число")
+          break;
+      } 
+    }
+   // data.push({ number, content });
+   index ? data[index].text = text : data.push({ number, content });
+   data.sort((a, b) => a.number - b.number);
   dataCreator();
   this.reset();
 });
